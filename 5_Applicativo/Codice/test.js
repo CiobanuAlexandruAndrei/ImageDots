7066e223b88fb5a314652cc728ae2c0d17fa4f4d
CiobanuAlexandruAndrei <alexandru.ciobanu@samtrevano.ch>
2023-09-15 15:33:54 +0200

    Selezione puntini

--- 5_Applicativo/Codice/test.js
+++ 5_Applicativo/Codice/test.js
@@ -1,4 +1,8 @@
 
+var dotsNum = 1;
+var dots = [];
+var movedImage = null;
+
 class Dot {
     constructor() {
         this.x = 0;
@@ -57,13 +61,61 @@ function addDot(event){
     dot.label = dotsNum;
     dot.draw(context);
 
+    dots.push(dot);
     dotsNum++;
 
     oneDot = dot;
 }
 
+function moveDot(){
+    // let x = 0, y = 0;
+    // let isMouseDown = false;
+
+    // const stopDrawing = () => { isMouseDown = false; }
+    // const startDrawing = event => {
+    //     isMouseDown = true;
+    //     [x, y] = [event.offsetX, event.offsetY];
+    // }
+    // const drawLine = event => {
+    //     if (isMouseDown) {
+    //         const newX = event.offsetX;
+    //         const newY = event.offsetY;
+    //         context.beginPath();
+    //         context.moveTo(x, y);
+    //         context.lineTo(newX, newY);
+    //         context.stroke();
+    //         //[x, y] = [newX, newY];
+    //         x = newX;
+    //         y = newY;
+    //     }
+    // }
+
+    // paintCanvas.addEventListener('mousedown', startDrawing);
+    // paintCanvas.addEventListener('mousemove', drawLine);
+    // paintCanvas.addEventListener('mouseup', stopDrawing);
+    // paintCanvas.addEventListener('mouseout', stopDrawing);
+}
+
+function selectDot(event){
+    var rect = canvas.getBoundingClientRect();
+    var scaleX = canvas.width / rect.width;
+    var scaleY = canvas.height / rect.height;
+
+    var x = Math.round((event.x - rect.left) * scaleX);
+    var y = Math.round((event.y - rect.top) * scaleY);
+
+    for(var dot of dots){
+        if (Math.abs(dot.x - x) < 10 && Math.abs(dot.y - y) < 10 ){
+            console.log(dot.label + " schiacciato");
+            break;
+        }
+    }
+}
+
 function selectFreeMode(){
     canvas.removeEventListener('mousedown', addDot);
+    canvas.addEventListener('mousedown', selectDot);
+
     var e = document.getElementById("free-mode-option");
     e.style.background = "#393E46";
     e.getElementsByTagName('img')[0].src = "Img/CursorLight.png";
@@ -76,10 +128,10 @@ function selectFreeMode(){
     e3.style.background = "#F7F7F7";
     e3.getElementsByTagName('img')[0].src = "Img/SelectDark.png";
 
-    
 }
 
 function selectAddMode(){
+    canvas.removeEventListener('mousedown', selectDot);
     canvas.addEventListener('mousedown', addDot);
     var e = document.getElementById("free-mode-option");
     e.style.background = "#F7F7F7";
@@ -104,7 +156,5 @@ canvas.style.backgroundSize = 'contain';
 canvas.width = img.width;
 canvas.height = img.height;
 
-var dotsNum = 1;
 
-var oneDot = null;
 
