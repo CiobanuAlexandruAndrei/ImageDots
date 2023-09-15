bb8415ae3d03a8e313a987c2e4b77f40b12aa2ee
CiobanuAlexandruAndrei <alexandru.ciobanu@samtrevano.ch>
2023-09-15 14:49:19 +0200

    Cambio tra selezione e puntini

--- 5_Applicativo/Codice/test.js
+++ 5_Applicativo/Codice/test.js
@@ -1,6 +1,6 @@
 
-class Ball{
-    constructor(){
+class Dot {
+    constructor() {
         this.x = 0;
         this.y = 0;
         this.size = 5;
@@ -8,7 +8,7 @@ class Ball{
         this.color = '#000';
     }
 
-    draw(context){
+    draw(context) {
         if (this.color == null) {
             this.color = '#000';
         }
@@ -36,11 +36,62 @@ class Ball{
     }
 }
 
-function saveCanvas(){
+function saveCanvas() {
     var link = document.createElement('a');
     link.download = 'image.png';
     link.href = document.getElementById('workspace-canvas').toDataURL()
     link.click();
+}
+
+function addDot(event){
+    var rect = canvas.getBoundingClientRect();
+    var scaleX = canvas.width / rect.width;
+    var scaleY = canvas.height / rect.height;
+
+    var x = Math.round((event.x - rect.left) * scaleX);
+    var y = Math.round((event.y - rect.top) * scaleY);
+
+    var dot = new Dot();
+    dot.x = x;
+    dot.y = y;
+    dot.label = dotsNum;
+    dot.draw(context);
+
+    dotsNum++;
+
+    oneDot = dot;
+}
+
+function selectFreeMode(){
+    canvas.removeEventListener('mousedown', addDot);
+    var e = document.getElementById("free-mode-option");
+    e.style.background = "#393E46";
+    e.getElementsByTagName('img')[0].src = "Img/CursorLight.png";
+
+    var e2 = document.getElementById("add-mode-option");
+    e2.style.background = "#F7F7F7";
+    e2.getElementsByTagName('img')[0].src = "Img/DotDark.png";
+
+    var e3 = document.getElementById("select-mode-option");
+    e3.style.background = "#F7F7F7";
+    e3.getElementsByTagName('img')[0].src = "Img/SelectDark.png";
+
+    
+}
+
+function selectAddMode(){
+    canvas.addEventListener('mousedown', addDot);
+    var e = document.getElementById("free-mode-option");
+    e.style.background = "#F7F7F7";
+    e.getElementsByTagName('img')[0].src = "Img/CursorDark.png";
+
+    var e2 = document.getElementById("add-mode-option");
+    e2.style.background = "#393E46";
+    e2.getElementsByTagName('img')[0].src = "Img/DotLight.png";
+
+    var e3 = document.getElementById("select-mode-option");
+    e3.style.background = "#F7F7F7";
+    e3.getElementsByTagName('img')[0].src = "Img/SelectDark.png";
 }
 
 const canvas = document.getElementById("workspace-canvas");
@@ -48,27 +99,12 @@ const context = canvas.getContext('2d');
 
 var img = new Image();
 img.src = "Img/pika.png";
-canvas.style.backgroundImage = 'url("Img/pika.png")';
+canvas.style.backgroundImage = 'url("Img/pika.png") ';
+canvas.style.backgroundSize = 'contain';
 canvas.width = img.width;
 canvas.height = img.height;
 
-var ballsNum = 1;
+var dotsNum = 1;
 
-canvas.addEventListener('mousedown', function (e) {
-    var rect = canvas.getBoundingClientRect();
-    var scaleX = canvas.width / rect.width;    
-    var scaleY = canvas.height / rect.height;
-    
-    var x = Math.round((e.x - rect.left) * scaleX);
-    var y = Math.round((e.y - rect.top) * scaleY);
-
-    var ball = new Ball();
-    ball.x = x;
-    ball.y = y,
-    ball.label = ballsNum;
-    ball.draw(context);
-
-    ballsNum++;
-
-});
+var oneDot = null;
 
